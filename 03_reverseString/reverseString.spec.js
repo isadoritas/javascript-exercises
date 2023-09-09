9d1a3e63c95368d94e001d23e7dd42e864f79d1a
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-09 14:33:06 -0300

    Update reverseString.spec.js

--- 03_reverseString/reverseString.spec.js
+++ 03_reverseString/reverseString.spec.js
@@ -4,15 +4,4 @@ describe('reverseString', () => {
   test('reverses single word', () => {
     expect(reverseString('hello')).toEqual('olleh');
   });
-
-  test.skip('reverses multiple words', () => {
-    expect(reverseString('hello there')).toEqual('ereht olleh')
-  })
-
-  test.skip('works with numbers and punctuation', () => {
-    expect(reverseString('123! abc!')).toEqual('!cba !321')
-  })
-  test.skip('works with blank strings', () => {
-    expect(reverseString('')).toEqual('')
-  })
 });
