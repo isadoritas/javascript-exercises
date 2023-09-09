99b25522c38a89532d42f5a250dc0594609aca93
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-09 15:51:24 -0300

    Update sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,14 @@
-const sumAll = function() {
+const sumAll = function(number1, number2) {
+    let totalSum = 0;
 
+    if (number1 > number2) {
+        [number1, number2] = [number2, number1]; // Troca os valores para garantir que number1 seja o menor
+    }
+
+    for (let i = number1; i <= number2; i++) {
+        totalSum += i;
+    }
+    return totalSum;
 };
-
-// Do not edit below this line
 module.exports = sumAll;
+
