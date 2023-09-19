2f589e21893c671e184086dfc80cb246bbab3dc1
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-19 14:15:33 -0300

    add content to the functions

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
+const add = function(a ,b) {
+  return a + b;
 	
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
+const sum = function(array) {
+  return array.reduce((total, current) => total + current, 0);
+};
 	
+
+const multiply = function(a, b) {
+  return a * b;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a,b);
 };
 
 const factorial = function() {
-	
+  if (n === 0) {
+    return 1;
+  }
+  return n * recursiveFactorial(n - 1);
 };
 
 // Do not edit below this line
