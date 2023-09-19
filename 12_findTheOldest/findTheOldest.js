b5ed304bd74ce84deefef30053faa44a2d6ea527
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-19 15:49:19 -0300

    add findTheOldest function

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,29 @@
-const findTheOldest = function() {
+const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]
 
-};
+function findTheOldest(people) {
+   const oldest = people.sort(function(a, b) {
+        const lastPerson = a.yearOfDeath - a.yearOfBirth;
+        const nextPerson = b.yearOfDeath - b.yearOfBirth;
+        return lastPerson > nextPerson ? -1 : 1;
+})[0];
+return oldest;
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
