15766b4fc48c383c6519cb40d59c16b49be6045a
Isadora Rodrigues <isadoradorarodrigues@gmail.com>
2023-09-19 14:31:45 -0300

    check if the reversed word  it's the same as the normal word

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+// It removes characters that are not lowercase letters from 'a' to 'z' or numbers from '0' to '9'
+const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "")
+const newWord = word.split('').reverse().join('');
+return newWord === word;
 };
-
 // Do not edit below this line
 module.exports = palindromes;
